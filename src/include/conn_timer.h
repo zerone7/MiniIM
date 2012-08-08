9d31ce21aa9506249810f1a379715452a79ecfa7
Luan Yinglin <synmyth@gmail.com>
2012-08-08 10:51:31 +0800

    block SIGALRM in process context to avoid race condition

--- src/include/conn_timer.h
+++ src/include/conn_timer.h
@@ -2,6 +2,7 @@
 #define _CONN_TIMER_H_
 
 #include <stdint.h>
+#include <signal.h>
 #include "conn_define.h"
 #include "conn_list.h"
 #include "conn_allocator.h"
@@ -47,8 +48,9 @@ static inline struct list_head* get_timeout_list(struct conn_timer *timer)
 	return &timer->timer_slots[timer->current];
 }
 
-/* add a connection to timer */
-static inline void timer_add(struct conn_timer *timer, struct connection *conn)
+/* add a connection to timer, this function
+ * should not be called in process context */
+static inline void __timer_add(struct conn_timer *timer, struct connection *conn)
 {
 	assert(timer && conn);
 	int timeout_seconds = timer->current + CLIENT_TIMEOUT;
@@ -56,19 +58,52 @@ static inline void timer_add(struct conn_timer *timer, struct connection *conn)
 	list_add(&conn->timer_list, &timer->timer_slots[insert_index]);
 }
 
+/* add a connection to timer, called in process context */
+static inline void timer_add(struct conn_timer *timer,
+		struct connection *conn)
+{
+	sigset_t mask, oldmask;
+
+	/* block the SIGALRM signal before call __timer_add */
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGALRM);
+	sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
+	__timer_add(timer, conn);
+
+	/* unblock the SIGALRM signal after call __timer_add */
+	sigprocmask(SIG_SETMASK, &oldmask, NULL);
+}
+
 /* delete a connection from timer */
-static inline void timer_del(struct connection *conn)
+static inline void __timer_del(struct connection *conn)
 {
 	assert(conn);
 	list_del(&conn->timer_list);
+}
+
+/* delete a connection from timer, called in process context */
+static inline void timer_del(struct connection *conn)
+{
+	sigset_t mask, oldmask;
+
+	/* block the SIGALRM signal before call __timer_del */
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGALRM);
+	sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
+	__timer_del(conn);
+
+	/* unblock the SIGALRM signal after call __timer_del */
+	sigprocmask(SIG_SETMASK, &oldmask, NULL);
 }
 
 /* move a connection from one slot to another slot */
 static inline void timer_move(struct conn_timer *timer, struct connection *conn)
 {
 	assert(timer && conn);
-	timer_del(conn);
-	timer_add(timer, conn);
+	__timer_del(conn);
+	__timer_add(timer, conn);
 }
 
 void timer_init(struct conn_timer *timer);
