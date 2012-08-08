eb69b86d0593d1d165a2fa8a764046d68f5f200c
Kyle Luo <xiaoluo7@gmail.com>
2012-08-08 16:14:09 +0800

    prepare.sh add passwd

--- db/prepare/prepare.sh
+++ db/prepare/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mysql -u root < im_new_user.sql
-mysql -u root < im_database.sql
-mysql -u root < im_user.sql
-mysql -u root < im_contact.sql
-mysql -u root < im_offline_msg.sql
+mysql -u root -p < im_new_user.sql
+mysql -u root -p< im_database.sql
+mysql -u root -p < im_user.sql
+mysql -u root -p< im_contact.sql
+mysql -u root -p< im_offline_msg.sql
