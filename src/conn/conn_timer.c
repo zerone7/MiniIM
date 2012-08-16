b00e9800e0f31f9c1617be00a52a561f1eeac04e
Luan Yinglin <synmyth@gmail.com>
2012-08-16 15:59:02 +0800

    remove delay slots in timer

--- src/conn/conn_timer.c
+++ src/conn/conn_timer.c
@@ -12,32 +12,12 @@ void timer_init(struct conn_timer *timer)
 	/* initialize list head */
 	assert(timer);
 	timer->current = 0;
-	timer->max_slots = CLIENT_TIMEOUT + 8;
-	timer->delay = timer->max_slots - CLIENT_TIMEOUT - 1;
+	timer->max_slots = CLIENT_TIMEOUT + 1;
 	timer->timer_slots = malloc(sizeof(struct list_head) * timer->max_slots);
 	assert(timer->timer_slots);
 	for (i = 0; i < timer->max_slots; i++) {
 		INIT_LIST_HEAD(&timer->timer_slots[i]);
 	}
-}
-
-/* mark the connection as 'unsafe', so we can't find it before delete it */
-void timer_mark_dead(struct conn_server *server, struct connection *conn)
-{
-	struct conn_timer *timer = &server->timer;
-	if (!is_alive_conn(timer, conn)) {
-		return;
-	}
-
-	uint8_t current = timer->current;
-	/* delay 4 seconds to delete it */
-	uint8_t new_index = (timer->current + 4) % timer->max_slots;
-	conn->timer_slot = new_index;
-	timer_del(conn);
-	list_add_tail(&conn->timer_list, &timer->timer_slots[new_index]);
-	close(conn->sfd);
-	hset_erase(&server->fd_conn_map, &conn->sfd);
-	hset_erase(&server->uin_conn_map, &conn->uin);
 }
 
 /* this function will be called every second */
